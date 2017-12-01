68d6f55bcd107f8f180edc8f03f753a6a8024624
凹凸 <543484525@qq.com>
2017-12-01 16:14:05 +0800

    修改 app.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,16 @@
-// import Todo from './todo/todos.js';
 import './assets/app.scss';
 import './lib/mobile-util.js';
 import $ from 'jquery';
-import './lib/vconsole.min.js';
+// import './lib/vconsole.min.js';
 
 /* 限制浏览器上下滑动 */
 document.addEventListener('touchmove', function (e) {
 	e.preventDefault();
 },true);
+
+function app() {
+	console.log('-------Dom is loaded! running....--------');
+}
+
+// The DOM is now loaded
+$(() => app());
